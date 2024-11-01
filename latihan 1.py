d50883b66fb0e054aed38cedb2b075c5705f2864
Safii2002 <safiiwibowo55@gmail.com>
2024-11-01 15:27:54 +0700

    Add files via upload

---
+++ latihan 1.py
@@ -0,0 +1,13 @@
+print('|[PROGRAM MENAMPILKAN N BILANGAN ACAK LEBIH < 0.5|')
+
+
+import random
+
+
+n = int(input("Masukan nilai N : "))
+for i in range (n) :
+    a=random.uniform(0.0,0.5)
+    print ("Data ke : ",i+1, "=> ", a)
+
+
+    print("selesai")
